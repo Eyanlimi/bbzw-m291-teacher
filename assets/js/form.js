1d4368b9f1bfcfc78e53403634a5126424f177cf
ralphlaurin <105487841+ralphlaurin@users.noreply.github.com>
2022-06-17 13:29:58 +0200

    Add files via upload

--- assets/js/form.js
+++ assets/js/form.js
@@ -24,7 +24,8 @@ email.addEventListener("keyup", (event) => {
 
 submit.addEventListener("click", async (event) => {
     event.preventDefault()
-    const result = await databaseClient.insertInto("users", ["email"], [email.value])
+    const result = await databaseClient.insertInto("users", ["email", "fname", "lname", "number"], [email.value, fname.value, lname.value, number.value])
+
     if (result.error) {
         alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
     }
